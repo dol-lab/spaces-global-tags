4d129813b2b85f4f53bdc28ea0d64ee0c197711d
Silvan Hagen <neverything@gmail.com>
2019-07-25 15:42:51 +0200

    Added activation routine, fixes #1

--- spaces-global-tags.php
+++ spaces-global-tags.php
@@ -12,8 +12,59 @@
  * @package         Spaces_Global_Tags
  */
 
+/**
+ * Namespace Definition.
+ */
 namespace Spaces_Global_Tags;
 
-function init() {
-	// Do stuff.
+/**
+ * Register our custom activation hook.
+ *
+ * @since 0.1.0
+ */
+register_activation_hook( __FILE__, __NAMESPACE__ . '\plugin_activate' );
+
+/**
+ * Plugin activation hook.
+ * Checks if it's a multiste and the necessary dependencies exist.
+ *
+ * @since 0.1.0
+ */
+function plugin_activate() {
+	if ( ! is_multisite() ) {
+		set_transient( 'spaces_global_tags_not_multisite', true, 5 );
+	}
+
+	if ( ! function_exists( 'network_the_title' ) ) {
+		set_transient( 'spaces_global_tags_missing_dependency', true, 5 );
+	}
 }
+
+/**
+ * Checks if during activation any transients were set.
+ * Adds dismissable error notices, in case it's not a
+ * multisite or the necessary dependencies don't exists.
+ *
+ * @since 0.1.0
+ */
+function check_dependencies() {
+	if ( get_transient( 'spaces_global_tags_not_multisite' ) ) {
+		?>
+		<div class="notice-error notice is-dismissible">
+			<p><?php _ex( 'This plugin needs to be run on a WordPress multisite installation.', 'network admin notice on activation', 'spaces-global-tags' ); ?></p>
+		</div>
+		<?php
+		delete_transient( 'spaces_global_tags_not_multisite' );
+	}
+
+	if ( get_transient( 'spaces_global_tags_missing_dependency' ) ) {
+		?>
+		<div class="notice-error notice is-dismissible">
+			<p><?php _ex( 'This plugin needs the True Multisite Indexer plugin to work properly.', 'network admin notice on activation', 'spaces-global-tags' ); ?></p>
+		</div>
+		<?php
+		delete_transient( 'spaces_global_tags_missing_dependency' );
+	}
+}
+
+add_action( 'network_admin_notices', __NAMESPACE__ . '\check_dependencies' );
