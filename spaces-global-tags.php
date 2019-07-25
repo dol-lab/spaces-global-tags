9d756cdead39f0d1f0868ffd68610d28e0d64397
Silvan Hagen <neverything@gmail.com>
2019-07-25 15:01:42 +0200

    Updated plugin description

--- spaces-global-tags.php
+++ spaces-global-tags.php
@@ -2,9 +2,9 @@
 /**
  * Plugin Name:     Spaces Global Tags
  * Plugin URI:      https://github.com/dol-lab/spaces-global-tags/
- * Description:     WIP: Do not use
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Description:     WIP: Do not use yet. Experimental plugin to play around with True Multisite Indexer.
+ * Author:          Silvan Hagen
+ * Author URI:      https://silvanhagen.com
  * Text Domain:     spaces-global-tags
  * Domain Path:     /languages
  * Version:         0.1.0
