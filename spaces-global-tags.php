177fcf5609a44dbe3641ea96be63cad5d93cfb53
Silvan Hagen <neverything@gmail.com>
2019-07-25 15:00:29 +0200

    Initial commit

---
+++ spaces-global-tags.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:     Spaces Global Tags
+ * Plugin URI:      https://github.com/dol-lab/spaces-global-tags/
+ * Description:     WIP: Do not use
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     spaces-global-tags
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Spaces_Global_Tags
+ */
+
+namespace Spaces_Global_Tags;
+
+function init() {
+	// Do stuff.
+}
