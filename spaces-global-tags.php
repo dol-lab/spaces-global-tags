76830f96808f1a868aa32742fe837200fa64ff4c
Silvan Hagen <neverything@gmail.com>
2019-07-25 17:48:53 +0200

    Added demo function for network tag

--- spaces-global-tags.php
+++ spaces-global-tags.php
@@ -68,3 +68,41 @@ function check_dependencies() {
 }
 
 add_action( 'network_admin_notices', __NAMESPACE__ . '\check_dependencies' );
+
+/**
+ * A dummy template tag to display posts according to a tag.
+ * This is just to play with the possibilites.
+ *
+ * @param string $tag_slug A slug for a tag.
+ */
+function get_posts_by_tag_slug( string $tag_slug = '' ) {
+	if ( '' === $tag_slug ) {
+		return;
+	}
+	$args = [
+		'posts_per_page' => 50,
+		'tax_query'      => [
+			[
+				'taxonomy' => 'post_tag',
+				'field'    => 'slug',
+				'terms'    => esc_sql( $tag_slug ),
+			],
+		],
+	];
+
+	$network_query = new \Network_Query( $args );
+
+	if ( $network_query->have_posts() ) :
+		while ( $network_query->have_posts() ) :
+			$network_query->the_post();
+			$blog_info = get_blog_details( $network_query->post->BLOG_ID );
+
+			echo '<h2 id="post-' . esc_attr( $network_query->post->ID ) . '" class="blog-' . esc_attr( $network_query->post->BLOG_ID ) . '">';
+			echo '<a href="' . esc_html( network_get_permalink( $network_query->post->BLOG_ID, $network_query->post->ID ) ) . '">' . esc_html( $network_query->post->post_title ) . '</a>';
+			echo '<br><small>' . esc_html( $blog_info->blogname ) . '</small>';
+			echo '</h3>';
+
+		endwhile;
+	endif;
+	network_reset_postdata();
+}
