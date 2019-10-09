22eb19cef9f238f6b113a7ca56ab09253926d4ca
Silvan Hagen <neverything@gmail.com>
2019-10-09 11:23:04 +0200

    Clean up and simplify code, see #16

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,10 +1,16 @@
 /**
+ * Spaces Global Tags: Auto-complete tag lookup.
+ *
+ * @package Spaces_Global_Tags
+ */
+
+/**
  * Simple XHR function.
  *
  * @param url
  * @param cb
  */
-function remoteSearch(url, cb) {
+const remoteSearch = function(url, cb) {
 	let URL                = url;
 	xhr                    = new XMLHttpRequest();
 	xhr.onreadystatechange = function() {
@@ -19,51 +25,59 @@ function remoteSearch(url, cb) {
 	};
 	xhr.open( "GET", URL,true );
 	xhr.send();
-}
+};
 
+/**
+ * Tribute shared config for comment and post Tags.
+ *
+ * @type {{lookup: string, spaceSelectsMatch: boolean, fillAttr: string, menuItemTemplate: (function(*): string), trigger: string, selectTemplate: (function(*): string)}}
+ */
+let tributeConfig = {
+	trigger: '#',
+	lookup: 'name',
+	fillAttr: 'name',
+	selectTemplate: function(item) {
+		return '#' + item.original.name;
+	},
+	menuItemTemplate: function(item) {
+		return '#' + item.original.name;
+	},
+	spaceSelectsMatch: true
+};
+
+/**
+ * Tribute for comment Tags.
+ */
 let tributeComments = new Tribute(
-	{
-		trigger: '#',
-		values: function(url, cb) {
-			remoteSearch( SpacesGlobalTags.routes.commentTags, tags => cb( tags ) );
-		},
-		lookup: 'name',
-		fillAttr: 'name',
-		selectTemplate: function(item) {
-			return '#' + item.original.name;
-		},
-		menuItemTemplate: function(item) {
-			return '#' + item.original.name;
-		},
-		spaceSelectsMatch: true
-	}
+	Object.assign(
+		tributeConfig,
+		{ values: function(url, cb) { remoteSearch( SpacesGlobalTags.routes.commentTags, tags => cb( tags ) ); } }
+	)
 );
 
+/**
+ * Attach Tribute for comments to the comment field in WordPress.
+ */
 if (  document.body.contains( document.getElementById( "comment" ) ) ) {
 	tributeComments.attach( document.getElementById( "comment" ) );
 }
 
+/**
+ * Tribute for post Tags.
+ */
 let tributePosts = new Tribute(
-	{
-		trigger: '#',
-		values: function(url, cb) {
-			remoteSearch( SpacesGlobalTags.routes.postTags, tags => cb( tags ) );
-		},
-		lookup: 'name',
-		fillAttr: 'name',
-		selectTemplate: function(item) {
-			return '#' + item.original.name;
-		},
-		menuItemTemplate: function(item) {
-			return '#' + item.original.name;
-		},
-		spaceSelectsMatch: true
-	}
+	Object.assign(
+		tributeConfig,
+		{ values: function(url, cb) { remoteSearch( SpacesGlobalTags.routes.postTags, tags => cb( tags ) ); } }
+	)
 );
 
+/**
+ * Attach Tribute for posts to the CKEditor for posts and not pages.
+ */
 document.addEventListener(
 	'onEditorReady',
-	function( e ) {
+	function() {
 		tributePosts.attach( document.querySelector( "[data-type=post] .ck-editor__editable_inline" ) );
 	},
 	false
